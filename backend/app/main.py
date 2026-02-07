b9eeec3acbb9737fd8dbc96de23de578c15730a5
AbduAlrimali <AbduAlrimali@yahoo.com>
2026-02-07 20:03:24 +0200

    Removed docling service from backend, updated the requirements.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import FastAPI, APIRouter
-from app.routers.v1 import auth, doc, user
+from app.routers.v1 import auth, user
 from app.core.security import setup_middlewares
 from app.core.logger import configure_logging, LogLevels
 from app.core.rate_limiter import limiter
@@ -27,7 +27,7 @@ load_dotenv()
 v1_router = APIRouter(prefix="/v1")
 v1_router.include_router(auth.router)
 v1_router.include_router(user.router)
-v1_router.include_router(doc.router)
+# v1_router.include_router(doc.router)
 
 app.include_router(v1_router)
 # if(__name__ == "__main__"):
